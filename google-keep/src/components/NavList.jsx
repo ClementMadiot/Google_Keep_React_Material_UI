29f9be07c25888a08c385e3c573dcf860baa6b1c
ClementMadiot <clementmadiot09@gmail.com>
2024-01-22 16:00:14 +0100

    add json files

--- google-keep/src/components/NavList.jsx
+++ google-keep/src/components/NavList.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
-// import IconButton from '@mui/material/IconButton';
-// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import {List, ListItem, ListItemButton, ListItemIcon, ListItemText} from '@mui/material';
+import { NavListData } from '../Data/NavListData';
 
 const NavList = () => {
+
   return (
     <List>
-    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-      <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-        <ListItemButton
-          button key={text}>
-          <ListItemIcon>
-            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-          </ListItemIcon>
-          <ListItemText primary={text}/>
+    {NavListData.map(list => (
+      <ListItem disablePadding sx={{ display: 'block' }}  key={list.id}>
+        <ListItemButton button key={list} >
+          <ListItemIcon>{list.icon}</ListItemIcon>
+          <ListItemText primary={list.name}/>
         </ListItemButton>
       </ListItem>
     ))}
