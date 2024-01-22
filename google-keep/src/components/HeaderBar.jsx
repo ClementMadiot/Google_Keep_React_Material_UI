be58277a30cc803568d21462c4edb237f7e31c9b
ClementMadiot <clementmadiot09@gmail.com>
2024-01-22 14:10:34 +0100

    add btn collapse

--- google-keep/src/components/HeaderBar.jsx
+++ google-keep/src/components/HeaderBar.jsx
@@ -1,43 +1,32 @@
 import * as React from 'react';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AppBar from '@mui/material/AppBar';
-// import { styled, useTheme} from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import MuiDrawer from '@mui/material/Drawer';
-// import List from '@mui/material/List';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import Divider from '@mui/material/Divider';
-// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import {AppBar, Toolbar, Typography, IconButton, styled} from '@mui/material';
+import {Menu} from '@mui/icons-material';
+
+const Header = styled(AppBar)`
+  z-index: 1201;
+  background-color: #fff;
+  height: 70px;
+  box-shadow: inset 0 -1px 0 0 #dadce0;
+`
 
 
 
-
-
-const HeaderBar = ({ open, handleDrawer, AppBar }) => {
+const HeaderBar = ({ open, handleDrawer }) => {
   return (
-    <AppBar position="fixed" open={open}>
+    <Header open={open}>
     <Toolbar>
       <IconButton
-        color="inherit"
-        aria-label="open drawer"
         onClick={handleDrawer}
         edge="start"
-        sx={{
-          marginRight: 5,
-          ...(open && { display: 'none' }),
-        }}
+        sx={{marginRight: '20px'}}
       >
-        <MenuIcon />
+        <Menu />
       </IconButton>
       <Typography variant="h6" noWrap component="div">
         Mini variant drawer
       </Typography>
     </Toolbar>
-  </AppBar>
+  </Header>
   );
 };
 
--- google-keep/src/components/SwipeDrawer.jsx
+++ google-keep/src/components/SwipeDrawer.jsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import { styled, useTheme} from '@mui/material/styles';
+import { styled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 // Components
 import HeaderBar from './HeaderBar';
@@ -43,25 +39,6 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 }));
 
 
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
     width: drawerWidth,
@@ -80,31 +57,22 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 );
 
 const SwipeDrawer = () => {
-  const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
+  const handleDrawer = () => {
+    setOpen(prevState => !prevState);
   };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   return (
     <Box sx={{ display: 'flex' }}>
       <HeaderBar
-      AppBar={AppBar}
       open={open}
-      handleDrawer={handleDrawerOpen}
+      handleDrawer={handleDrawer}
       />
       <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </DrawerHeader>
-      <NavList/>
+        <DrawerHeader></DrawerHeader>
+        <NavList/>
       </Drawer>
     </Box>
   );
